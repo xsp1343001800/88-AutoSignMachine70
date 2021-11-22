942d06cc31109d0a9713c6dc48d1668f463a4c4f
xsp1343001800 <xsp1343001800@qq.com>
2021-11-23 07:46:05 +0800

    11.23

--- utils/device.js
+++ utils/device.js
@@ -15,8 +15,8 @@ function generateMixed(n) {
 
 
 var appInfo = {
-    version: 8.0920,
-    unicom_version: 'android@8.0920',
+    version: 8.0902,
+    unicom_version: 'android@8.0902',
     app_name: '中国联通',
     package_name: 'com.sinovatech.unicom.ui'
 }
